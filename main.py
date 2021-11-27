937fbfd9aea930ad1d24d09c906ceb9756a1973e
gareth <61771862+GsnailG@users.noreply.github.com>
2021-11-27 20:51:54 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("we have logged in as {0.user}"
+    .format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return #ignores its own messages
+
+    if message.content.startswith("hello"):
+        await message.channel.send("Hello!")
+
+    if message.content.startswith("britain"):
+        await message.channel.send(":flag_gb:")
+
+
+  
+
+client.run("")
